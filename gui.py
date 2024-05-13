c3542d7863c3ee6b78cf3fd06be0d04b20f06c71
SolarRaydiation <ray.abenido@gmail.com>
2024-05-13 22:57:56 +0800

    Implemented function GUI of pwdgenerator.py

--- gui.py
+++ gui.py
@@ -1,72 +1,134 @@
-"""
-everything in tkinter is a widget
-everything on kinter is a two step proces: you have to define the thing and then make it show up on the screen
-pack - shoving it into TK
-
-"""
-
 import customtkinter
 import math
 from pwdgenerator import create_password
 
 class PasswordGenerator(customtkinter.CTk):
     def __init__(self):
+        super().__init__()
+
         # declare root window
-        app = customtkinter.CTk()
-        app.title("my app")
-        app.geometry("600x500")
-        app.minsize(600, 500)
-        # prevents column from collapse to the size of the widget inside.
-        app.grid_columnconfigure(0, weight=1)
+        self.app = customtkinter.CTk()
+        self.app.title("my app")
+        self.app.geometry("600x500")
+        self.app.minsize(600, 500)
+        # prevents column from collapsing to the size of the widget inside.
+        self.app.grid_columnconfigure(0, weight=1)
+
+        # declare the StringVars
+        # note: variables declared first or interpreter will complain
+        self.use_digits = customtkinter.StringVar(value="on")
+        self.use_uppercase = customtkinter.StringVar(value="on")
+        self.use_lowercase = customtkinter.StringVar(value="on")
+        self.use_sc = customtkinter.StringVar(value="on")
+        self.char_length = 12 # default password length
+
 
         # declare the widgets for the window
-        # TO-DO: clean up to be PEP8 compliant
-        title = customtkinter.CTkLabel(app, text='PASSWORD GENERATOR', font=('Roboto', 30))
-        subtitle = customtkinter.CTkLabel(app, font=('Roboto', 15), text='Generate a random and cryptographically secure password below!')
-        subtitle.bind( '<Configure>', lambda e: subtitle.configure(wraplength=subtitle.winfo_width()))
-        char_count = customtkinter.CTkLabel(app, text='6 characters')
-        slider = customtkinter.CTkSlider(app, from_=0, to=30, command=lambda e: char_count.configure(text=f'{math.floor(e)} characters'))
-        password_field = customtkinter.CTkEntry(app, placeholder_text="Password goes here!")
-        btn_copy = customtkinter.CTkButton(app, text='Copy', command=lambda e: print('Copy!'))
-
-        # declare the variables for the checkboxes below
-        # note: variables declared first or interpreter will complain 
-        use_digits = customtkinter.StringVar(value="on")
-        use_uppercase = customtkinter.StringVar(value="on")
-        use_lowercase = customtkinter.StringVar(value="on")
-        use_sc = customtkinter.StringVar(value="on")
-        char_length = 12 # default password length
+        self.title = customtkinter.CTkLabel(
+            master=self.app,
+            text='PASSWORD GENERATOR',
+            font=('Roboto', 30))
+        self.subtitle = customtkinter.CTkLabel(
+            master=self.app,
+            font=('Roboto', 15),
+            text='Generate a random and cryptographically secure password below!')
+        self.subtitle.bind(
+            '<Configure>',
+            lambda e: self.subtitle.configure(wraplength=self.subtitle.winfo_width()))
+        self.char_length_display = customtkinter.CTkLabel(
+            master=self.app,
+            text=f'{self.char_length} characters')
+        self.slider = customtkinter.CTkSlider(
+            master=self.app,
+            from_=1, to=30,
+            command=self._update_char_length_display) 
+        self.password_field = customtkinter.CTkEntry(
+            master=self.app,
+            placeholder_text="Password goes here!")
+        self.btn_copy = customtkinter.CTkButton(
+            master=self.app,
+            text='Copy',
+            command=self._copy_to_clipboard)
 
         # delcare the checkboxes
-        # TO-DO: clean up to be PEP8 compliant
-        digit_checkbox = customtkinter.CTkCheckBox(app, text="Use digits?", command=None,
-                                            variable=use_digits, onvalue="on", offvalue="off")
-        uppercase_checkbox = customtkinter.CTkCheckBox(app, text="Use uppercase letters?", command=None,
-                                            variable=use_uppercase, onvalue="on", offvalue="off")
-        lowercase_checkbox = customtkinter.CTkCheckBox(app, text="Use lowercase letters?", command=None,
-                                            variable=use_lowercase, onvalue="on", offvalue="off")
-
-        sc_checkbox = customtkinter.CTkCheckBox(app, text="Use special characters?", command=None,
-                                            variable=use_sc, onvalue="on", offvalue="off")
+        self.digit_checkbox = customtkinter.CTkCheckBox(
+            self.app, text="Use digits?",
+            command= self._generate_password,
+            variable=self.use_digits,
+            onvalue="on",
+            offvalue="off")
+        self.uppercase_checkbox = customtkinter.CTkCheckBox(
+            self.app, text="Use uppercase letters?",
+            command= self._generate_password,
+            variable=self.use_uppercase,
+            onvalue="on", offvalue="off")
+        self.lowercase_checkbox = customtkinter.CTkCheckBox(
+            self.app, text="Use lowercase letters?",
+            command= self._generate_password,
+            variable=self.use_lowercase,
+            onvalue="on", offvalue="off")
+        self.sc_checkbox = customtkinter.CTkCheckBox(
+            self.app, text="Use special characters?",
+            command= self._generate_password,
+            variable=self.use_sc, onvalue="on", offvalue="off")
 
         # arrange the grid
-        title.grid(row=0, column=0, padx=20, pady=20, stick='ew', columnspan=3)
-        subtitle.grid(row=1, column=0, padx=20, pady=20, rowspan=2, stick='ew', columnspan=3)
-        password_field.grid(row=3, column=0, padx=20, rowspan=2, stick='ew', columnspan=2)
-        btn_copy.grid(row=3, column=2, padx=20, stick='ew')
-        slider.grid(row=5, column=0, padx=20, pady=20, stick='ew', columnspan=2)
-        char_count.grid(row=5, column=2, padx=40, pady=20, stick='ew')
-        digit_checkbox.grid(row=6, column=0, padx=40, pady=20, stick='ew')
-        uppercase_checkbox.grid(row=7, column=0, padx=40, pady=20, stick='ew')
-        lowercase_checkbox.grid(row=8, column=0, padx=40, pady=20, stick='ew')
-        sc_checkbox.grid(row=9, column=0, padx=40, pady=20, stick='ew')
+        self.title.grid(row=0, column=0, padx=20, pady=20, stick='ew', columnspan=3)
+        self.subtitle.grid(row=1, column=0, padx=20, pady=20, rowspan=2, stick='ew', columnspan=3)
+        self.password_field.grid(row=3, column=0, padx=20, rowspan=2, stick='ew', columnspan=2)
+        self.btn_copy.grid(row=3, column=2, padx=20, stick='ew')
+        self.slider.grid(row=5, column=0, padx=20, pady=20, stick='ew', columnspan=2)
+        self.char_length_display.grid(row=5, column=2, padx=40, pady=20, stick='ew')
+        self.digit_checkbox.grid(row=6, column=0, padx=40, pady=20, stick='ew')
+        self.uppercase_checkbox.grid(row=7, column=0, padx=40, pady=20, stick='ew')
+        self.lowercase_checkbox.grid(row=8, column=0, padx=40, pady=20, stick='ew')
+        self.sc_checkbox.grid(row=9, column=0, padx=40, pady=20, stick='ew')
 
-        app.mainloop()
+        self.app.mainloop()
+        self._generate_password()
+
+    def _update_char_length_display(self, value):
+        """ Updates the `char_lengt_display` widget to show the number of
+        characters the generated password will produce.
+        """
+        self.char_length = math.floor(value)
+        self.char_length_display.configure(text=f'{self.char_length} characters')
+        self._generate_password()
+
+    def _generate_password(self):
+        """A wrapper function for `create_password()` in the `pwdgenerator.py` script.
+        """
+        try:
+            generated_password = create_password(
+                    self._parse_StringVar(self.use_lowercase.get()),
+                    self._parse_StringVar(self.use_uppercase.get()),
+                    self._parse_StringVar(self.use_digits.get()),
+                    self._parse_StringVar(self.use_sc.get()),
+                    self.char_length)
+            print(generated_password)
+            # the docs don't make it obvious but the delete()'s last index
+            # arg is exclusive and not inclusive
+            self.password_field.delete(0,len(self.password_field.get()))
+            self.password_field.insert(0, generated_password)
+        except:
+            self.password_field.delete(0,len(self.password_field.get()))
+            self.password_field.insert(0, "Ran into error while generating password!")
+        
+        print(self.char_length)
     
-    def set_password():
-        pass
+    def _copy_to_clipboard(self):
+        # NOTE: arg is NOT needed and ignored
+        self.app.clipboard_append(self.password_field.get())
 
-    def copy_to_clipboard():
-        pass
+    def _parse_StringVar(self, string: str) -> bool:
+        """Converts StringVar instances to their boolean equivalent.
+        """
+        match(string):
+            case 'on':
+                return True
+            case 'off':
+                return False
+            case _:
+                raise Exception(f'Case {string} not found while parsing StringVar')
 
 root = PasswordGenerator()
